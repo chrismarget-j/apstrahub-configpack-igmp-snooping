2b4cc33d1dda302f9299da0ba559c4ba8e94f80d
Chris Marget <cmarget@juniper.net>
2025-07-30 14:22:25 -0400

    initial commit

--- pack/README.md
+++ pack/README.md
@@ -1,5 +1,3 @@
-# example
+# igmp-snooping
 
-This pack is primarily a template intended to help pack publishers put together their own packs and pack release workflows.
-
-It a simple example which creates a single _configlet_ in the Apstra global catalog.
+Enables igmp snooping for any vlan with 'mcast' in the vlan name when applied to leaf nodes
--- pack/tofu/configlet.tf
+++ pack/tofu/configlet.tf
@@ -2,20 +2,24 @@
 #  All rights reserved.
 #  SPDX-License-Identifier: MIT
 
-resource "apstra_configlet" "example" {
+resource "apstra_datacenter_configlet" "example" {
+  blueprint_id = var.blueprint_id
   name = var.name
+  condition = "role in [\"leaf\", \"access\"]"
   generators = [
     {
       config_style  = "junos"
       section       = "top_level_hierarchical"
       template_text = <<-EOT
-        ether-options {
-          802.3ad {
-            lacp {
-              force-up;
-            }
+        {% for vlan, vlan_info in vlan.items() %}
+        {% if 'mcast' in vlan_info.name %}
+        protocols {
+          igmp-snooping {
+            vlan vn{{ vlan_info.id }}
           }
         }
+        {% endif %}
+        {% endfor %}
       EOT
     },
   ]
